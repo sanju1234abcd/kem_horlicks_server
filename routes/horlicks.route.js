fba1867ebe24d6f9733ae3038747e9ac7f02d604
sanju marik <jackiehsj32@gmail.com>
2025-09-27 05:39:01 +0530

    test 2

--- routes/horlicks.route.js
+++ routes/horlicks.route.js
@@ -6,6 +6,7 @@ const horlicksRouter = express.Router()
 horlicksRouter.post("/add", addHorlicksSales );
 horlicksRouter.get("/get", getHorlicksSales );
 horlicksRouter.get("/health",async(req,res)=>{
+    console.log("Healthy")
     res.status(200).json({success:true, message:"Healthy"})
 })
 
